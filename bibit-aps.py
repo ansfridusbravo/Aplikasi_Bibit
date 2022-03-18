1ca2f54b7387e9c345989623e4f1e549c7363bf1
ansfridusbravo <55519345+ansfridusbravo@users.noreply.github.com>
2022-03-18 18:25:13 +0700

    Add files via upload

--- bibit-aps.py
+++ bibit-aps.py
@@ -4,6 +4,8 @@ import numpy as np
 import pickle
 from sklearn.naive_bayes import GaussianNB
 from PIL import Image
+
+
 image = Image.open('gambar1.png')
 st.image(image)
 # --server.port 80
@@ -27,18 +29,17 @@ def user_input_features():
     Pruning = st.sidebar.slider('Pruning (Semester)',1,3,0)
     PengendalianLalang = st.sidebar.slider('Pengendalian Lalang (Semester)',1,4,0)
     GarukPiringan = st.sidebar.slider('Garuk Piringan(Semester)',1,4,0)
-    PenyemprotanPiringan = st.sidebar.slider('Penyemprotan Piringan (Semester)',1,4,0)
     BTP = st.sidebar.slider('BTP (Semester)',1,4,0)
-    BabatSemak = st.sidebar.slider('Babat Semak(Semester)',1,4,0)
-    Urea = st.sidebar.slider('Urea (KG)/Batang',0.0,3.0,0.0)
-    MOP = st.sidebar.slider('MOP (KG)/Batang',0.0,3.0,0.0)
-    Dolomite = st.sidebar.slider('Dolomite (KG)/Batang',0.0,2.0,0.0)
-    ZNSO4 = st.sidebar.slider('Zinc Sulphate (ZnSO4) (KG)/Batang',0.0,2.0,0.0)
-    CUSO4 = st.sidebar.slider('Copper Sulphate (CuSO4) (KG)/Batang',0.0,2.0,0.0)
-    RockPhosphate = st.sidebar.slider('Rock Phosphat (KG)/Batang',0.0,2.0,0.0)
-    NPK = st.sidebar.slider('NPK (KG)/Batang',0.0,3.0,0.0)
-    HGFP = st.sidebar.slider('HGFP (KG)/Batang',0.0,1.0,0.0)
-    TSP = st.sidebar.slider('TSP (KG)/Batang',0.0,1.0,0.0)
+    
+    st.sidebar.header('Pengaplikasian Pupuk')
+    Urea = st.sidebar.slider('Urea (KG)/Pokok',0.0,3.0,0.0)
+    MOP = st.sidebar.slider('MOP (KG)/Pokok',0.0,3.0,0.0)
+    Dolomite = st.sidebar.slider('Dolomite (KG)/Pokok',0.0,2.0,0.0)
+    ZNSO4 = st.sidebar.slider('Zinc Sulphate (ZnSO4) (KG)/Pokok',0.0,2.0,0.0)
+    CUSO4 = st.sidebar.slider('Copper Sulphate (CuSO4) (KG)/Pokok',0.0,2.0,0.0)
+    RockPhosphate = st.sidebar.slider('Rock Phosphat (KG)/Pokok',0.0,2.0,0.0)
+    NPK = st.sidebar.slider('NPK (KG)/Pokok',0.0,3.0,0.0)
+    TSP = st.sidebar.slider('TSP (KG)/Pokok',0.0,1.0,0.0)
     data = {
             'JenisTanah' : JenisTanah,
             'JenisDataran':JenisDataran,
@@ -48,9 +49,7 @@ def user_input_features():
             'Pruning':Pruning,
             'PengendalianLalang': PengendalianLalang,
             'GarukPiringan' : GarukPiringan,
-            'PenyemprotanPiringan' : PenyemprotanPiringan,
             'BTP' : BTP,
-            'BabatSemak' : BabatSemak,
             'Urea' : Urea,
             'MOP' : MOP,
             'Dolomite' : Dolomite,
@@ -58,14 +57,13 @@ def user_input_features():
             'CUSO4' : CUSO4,
             'RockPhosphate' : RockPhosphate,
             'NPK' : NPK,
-            'HGFP' : HGFP,
             'TSP' : TSP
             }
     features = pd.DataFrame(data, index=[0])
     return features
 input_df = user_input_features()
 
-st.subheader('Data Kondisi Lahan dan Perawatan yang telah diisi')
+st.subheader('Data Kondisi Lahan dan Perawatan yang telah dimasukkan')
 st.write(input_df)
 
 input_df['JenisTanah'] = input_df['JenisTanah'].replace("Pasir", "1")
@@ -83,26 +81,7 @@ input_df['KelasDrainase'] = input_df['KelasDrainase'].replace("Sedang", "3")
 input_df['KelasDrainase'] = input_df['KelasDrainase'].replace("Terhambat", "4")
 
 # kombinasi user input dengan masukan dataset
-df = pd.read_csv('dataset.csv')
-
-#df.drop(['NamaBlok'],axis=1,inplace=True)
-
-df['JenisTanah'] = df['JenisTanah'].replace("Pasir", "1")
-df['JenisTanah'] = df['JenisTanah'].replace("Mineral", "2")
-
-df['JenisTanah'] = df['JenisTanah'].replace(" Pasir ", "1")
-df['JenisTanah'] = df['JenisTanah'].replace(" Mineral ", "2")
-
-df['JenisDataran'] = df['JenisDataran'].replace("DataranRendah", "1")
-df['JenisDataran'] = df['JenisDataran'].replace("Rawa-rawa", "2")
-
-df['KondisiDataran'] = df['KondisiDataran'].replace("Miring", "1")
-df['KondisiDataran'] = df['KondisiDataran'].replace("Datar", "2")
-
-df['KelasDrainase'] = df['KelasDrainase'].replace("TidakAda", "1")
-df['KelasDrainase'] = df['KelasDrainase'].replace("Lancar", "2")
-df['KelasDrainase'] = df['KelasDrainase'].replace("Sedang", "3")
-df['KelasDrainase'] = df['KelasDrainase'].replace("Terhambat", "4")
+df = pd.read_csv('DatasetSmoote2.csv',index_col="Unnamed: 0")
 
 X = df.drop('JenisBibit', axis=1)
 y = df['JenisBibit']
@@ -113,6 +92,15 @@ clf.fit(X, y)
 prediction = clf.predict(input_df)
 prediction_proba = clf.predict_proba(input_df)
 
-st.subheader('Prediksi Bibit :')
-st.write(prediction)
+# Menampilkan label dengan nomor indeksnya
+st.subheader('Daftar Varietas Bibit Yang Diprediksi')
+st.write('1. Varietas ASD')
+st.write('2. Varietas SRJ (Sriwijaya)')
+
+# menampilkan probabilitas prediksi
+st.subheader(
+'Nilai peluang setiap varietas bibit')
+st.write(pd.DataFrame(clf.predict_proba(input_df),columns=clf.classes_))
+
+st.write('Berdasarkan dari nilai peluang diatas, maka varietas bibit kelapa sawit yang diprediksi sesuai dengan kondisi lahan dan perawatan tersebut adalah Varietas',(prediction[0]))
 
